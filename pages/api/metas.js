99b80536156e2898857abfbf1fe744aebfbc0b55
Ankan Bag <bagankan@gmail.com>
2022-08-07 21:55:13 +0530

    add: api route to scrape meta tags from url

---
+++ pages/api/metas.js
@@ -0,0 +1,31 @@
+const puppeteer = require("puppeteer");
+
+export default async function handler(req, res) {
+  const { q: url } = req.query;
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    const meta = await page.evaluate(() => {
+      const ogMetas = {};
+      Array.from(document.getElementsByTagName("meta")).forEach((meta) => {
+        const metaProp = meta.getAttribute("property");
+        if (!!metaProp) {
+          ogMetas[metaProp] = meta.getAttribute("content");
+        }
+      });
+      return ogMetas;
+    });
+    await browser.close();
+    res.status(200).json({
+      error: false,
+      msg: Object.keys(meta).length
+        ? "Successfully fetched meta data"
+        : "No meta data found",
+      data: meta,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: true, msg: "Error fetching meta data" });
+  }
+}
